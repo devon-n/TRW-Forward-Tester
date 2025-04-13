e6e271cad277e9317cdf33d987e7a268030efd32
Dev Nathan <65629705+devon-n@users.noreply.github.com>
2025-04-13 06:25:19 +0400

    Update dashboard.py
    Added error message if no trades are found

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -31,7 +31,9 @@ def get_data_from_mongodb():
 df = get_data_from_mongodb()
 print('Saving CSV')
 df.to_csv('test.csv', index=False)
-
+if len(df) == 0:
+    print("No trades in database check webhook settings, network access for database, .env file and render environment variables are correct")
+    sys.exit()
 
 df['Time'] = pd.to_datetime(df['time'])
 df['close'] = pd.to_numeric(df['close'])
