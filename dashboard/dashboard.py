b885134237a4c12880fdba3998c7c394d631c318
Dev Nathan <65629705+devon-n@users.noreply.github.com>
2025-05-10 06:04:01 +0400

    chore: print message for no trades

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -32,6 +32,9 @@ df = get_data_from_mongodb()
 print('Saving CSV')
 df.to_csv('test.csv', index=False)
 
+if len(df) == 0:
+    print("\n\nNo trades in database check webhook settings, network access for database, .env file and render environment variables are correct\n\n")
+    sys.exit()
 
 df['Time'] = pd.to_datetime(df['time'])
 df['close'] = pd.to_numeric(df['close'])
