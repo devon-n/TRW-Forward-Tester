65acdb2121855d8c9ab2fc4ae6ff483d3b26693c
Jayhuue <166196485+jaydude85@users.noreply.github.com>
2025-05-27 10:12:11 +0900

    Fixed Execution Error

--- app.py
+++ app.py
@@ -267,26 +267,26 @@ def execute_order(data):
             #client.futures_change_margin_type(symbol=ticker, marginType="ISOLATED")
             print(f"\nSending Order: {json.dumps(data)}\n")
             if side == "Buy":
-                order_params = session.place_order(
+                order_params = {
                     category = "linear",
                     symbol = ticker,
                     side = side,
                     orderType = "Limit",
                     qty = quantity,
                     price = str(float(order_price)-0.01)
-                    )
+                }
                 if stop_loss != "na":
                     order_params["stopLoss"] = stop_loss
                 order_response = session.place_order(**order_params)
             if side == "Sell":
-                order_params = session.place_order(
+                order_params = {
                     category = "linear",
                     symbol = ticker,
                     side = side,
                     orderType = "Limit",
                     qty = quantity,
                     price = str(float(order_price)-0.01)
-                    )
+                }
                 if stop_loss != "na":
                     order_params["stopLoss"] = stop_loss
                 order_response = session.place_order(**order_params)
