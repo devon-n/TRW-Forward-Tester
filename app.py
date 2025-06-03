e5d3ad40663fa83e36b6a813c68527878346cd1a
Jaehyun Kang <dreemdex@gmail.com>
2025-06-04 07:37:41 +0900

    Tried removing additional opening spreadsheet

--- app.py
+++ app.py
@@ -169,11 +169,10 @@ def execute_order(data):
                 current_date = datetime.now(timezone.utc).strftime("%d/%m/%Y")
                 current_time = datetime.now(timezone.utc).strftime("%H:%M:%S")
                 # Open the spreadsheet and worksheet
-                try:
-                    spreadsheet = client.open("Live Trading")
-                    sheet = spreadsheet.worksheet("Order History (A)")
-                except Exception as e:
-                    print(f"Error while creating spreadsheet {e}")
+
+                spreadsheet = client.open("Live Trading")
+                sheet = spreadsheet.worksheet("Order History (A)")
+
 
                 # Sample data to append
                 order_data = [
@@ -243,7 +242,7 @@ def execute_order(data):
                 current_date = datetime.now(timezone.utc).strftime("%d/%m/%Y")
                 current_time = datetime.now(timezone.utc).strftime("%H:%M:%S")
                 # Open the spreadsheet and worksheet
-                spreadsheet = client.open("Live Trading")
+                #spreadsheet = client.open("Live Trading")
                 sheet = spreadsheet.worksheet("Trade History (A)")
 
                 # Sample data to append
@@ -377,7 +376,7 @@ def execute_order(data):
                 current_date = datetime.now(timezone.utc).strftime("%d/%m/%Y")
                 current_time = datetime.now(timezone.utc).strftime("%H:%M:%S")
                 # Open the spreadsheet and worksheet
-                spreadsheet = client.open("Live Trading")
+                #spreadsheet = client.open("Live Trading")
                 sheet = spreadsheet.worksheet("Trade History (A)")
 
                 # Sample data to append
@@ -433,7 +432,7 @@ def execute_order(data):
                 current_date = datetime.now(timezone.utc).strftime("%d/%m/%Y")
                 current_time = datetime.now(timezone.utc).strftime("%H:%M:%S")
                 # Open the spreadsheet and worksheet
-                spreadsheet = client.open("Live Trading")
+                #spreadsheet = client.open("Live Trading")
                 sheet = spreadsheet.worksheet("PnL (A)")
 
                 # Sample data to append
