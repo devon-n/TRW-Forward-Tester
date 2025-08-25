689b1f2d5dfa8a3c02d7e0cc0fc08d35b6cfd2fc
naidenpetrov00 <naiden.petrov.31.12.00@gmail.com>
2025-08-25 22:58:53 +0300

    chenge doge minQty

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,4 @@
-minQtyDict = {"BTCUSDT": 0.001, "ETHUSDT": 0.01, "DOGEUSDT": 22}
+minQtyDict = {"BTCUSDT": 0.001, "ETHUSDT": 0.01, "DOGEUSDT": 24}
 
 precisionDecimalDict = {"BTCUSDT": 3, "ETHUSDT": 2, "DOGEUSDT": 0}
 
