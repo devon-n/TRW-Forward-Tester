65b19fa089275a71d075bc5ff072b45b1b4314a9
silentninjax <gab.phn@proton.me>
2026-09-03 19:56:52 +0800

    Updated values

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 minQtyDict = {
-    "ARBUSDT": "9",
-    "BTCUSDT": "0.002",
-    "AVAXUSDT": "2",
-    "1000PEPEUSDT": "700"
+    "BTCUSDT": "0.001"
 }
 
 precisionDecimalDict = {
-    "1000SHIBUSDT": 0,
-    "1000PEPEUSDT": 0,
-    "KAVAUSDT":1,
-    "ARBUSDT": 1
+    "ARBUSDT": 3
 }
